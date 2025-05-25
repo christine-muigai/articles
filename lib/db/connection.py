32f1fdc72aa58d93070ad95fe68eeb4bb25e4fc3
christine-muigai <git@github.com>
2025-05-25 14:01:40 +0300

    Auto commit

--- lib/db/connection.py
+++ lib/db/connection.py
@@ -1,6 +1,10 @@
+import os
 import sqlite3
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  
+DB_PATH = os.path.join(BASE_DIR, 'articles.db')  
+
 def get_connection():
-    conn = sqlite3.connect('articles.db')
+    conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
