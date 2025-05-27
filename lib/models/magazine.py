9d905950916edcd0e0364da9615908f73ffe12e8
christine-muigai <git@github.com>
2025-05-27 08:31:53 +0300

    Auto commit

--- lib/models/magazine.py
+++ lib/models/magazine.py
@@ -1,6 +1,8 @@
 from lib.db.connection import get_connection
 from lib.models.article import Article
-from lib.models.author import Author
+
+def some_method():
+    from lib.models.author import Author
 
 class Magazine:
     def __init__(self, id, name, category):
--- tests/test_author.py
+++ tests/test_author.py
@@ -1,1 +1,6 @@
+from lib.models.author import Author
 
+def test_author_initialization():
+    author = Author(id=1, name="Alice")  # Pass both required arguments
+    assert author.id == 1
+    assert author.name == "Alice"
